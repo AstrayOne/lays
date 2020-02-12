8d3185dca235e69ea6b598679176206f84d085d6
Dmitry <fantomix293@gmail.com>
2020-02-12 18:36:47 +0300

    fixes

--- js/mobile-menu-animation.js
+++ js/mobile-menu-animation.js
@@ -1,40 +1,77 @@
 
-var flag = false;
+let flag = false;
 
-$('.header-logo__link').on('click', function() {
+let element = $(".header-logo__image");
+let element1 = $('.header-logo__link');
 
-    if ($(window).width() <= 680)
+if ($(window).width() <= 680)
+{
+
+    document.onclick = function(e)
     {
-        if(flag == false)
+        let link = event.target.closest('.header-logo__link');
+        
+        if (link)
         {
-            $('.header-menu-mobile').addClass('mobile-menu__visible');
-            $('.header-menu-mobile__item').addClass('mobile-menu__animate');
-            $('.mobile-menu__line_1').addClass('line-animate1');
-            $('.mobile-menu__line_2').addClass('line-animate2');
-            $('.mobile-menu__line_3').addClass('line-animate3');
-
-
-            flag = true;
+        
+            if(flag == false && ( event.target.className != 'header-logo__image'))
+            {
+                openMobileMenu();
+                
+            }
+            else
+            {
+                closeMobileMenu();
+                
+            }
         }
-        else
-        {
-            $('.header-menu-mobile').removeClass('mobile-menu__visible');
-            $('.header-menu-mobile__item').removeClass('mobile-menu__animate');
-            $('.mobile-menu__line_1').removeClass('line-animate1');
-            $('.mobile-menu__line_2').removeClass('line-animate2');
-            $('.mobile-menu__line_3').removeClass('line-animate3');
-            flag = false;
-        }
-    }
-});
+    };
 
-$('.mobile-menu__close').on('click', function() {
     
+    let scrollPos = 0;
+    $(window).scroll(function()
+    {
+        
+        let st = $(this).scrollTop();
+        if(flag == true)
+        {
+            if (st > scrollPos){
+                
+                closeMobileMenu();
+            }
+        }
+        scrollPos = st;
+
+    });
+    
+    $('.mobile-menu__close').on('click', function() {
+        closeMobileMenu();
+    });
+}
+
+
+
+function openMobileMenu() 
+{
+
+    $('.header-menu-mobile').addClass('mobile-menu__visible');
+    $('.header-menu-mobile__item').addClass('mobile-menu__animate');
+    $('.mobile-menu__line_1').addClass('line-animate1');
+    $('.mobile-menu__line_2').addClass('line-animate2');
+    $('.mobile-menu__line_3').addClass('line-animate3');
+
+    flag = true;
+  }
+
+function closeMobileMenu() 
+{
+
     $('.header-menu-mobile').removeClass('mobile-menu__visible');
     $('.header-menu-mobile__item').removeClass('mobile-menu__animate');
     $('.mobile-menu__line_1').removeClass('line-animate1');
     $('.mobile-menu__line_2').removeClass('line-animate2');
     $('.mobile-menu__line_3').removeClass('line-animate3');
     flag = false;
-    
-});
+}
+
+
--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,84 +1,33 @@
-if ($(window).width() > 680 && $(window).width() <= 800 )
+
+jQuery(document).ready(function($) 
+{  
+
+    $(window).scroll(function()
     {
-        
-        $('.prizes-about').addClass('scroll-animated');
-        $('.prizes-baloon').addClass('scroll-animated');
+        scrollAnimate($('.prizes-about'));
+        scrollAnimate($('.prizes-baloon'));
+        scrollAnimate($('.summer-promo'));
+        scrollAnimate($('.summer-map'));
 
+    });
+
+    scrollAnimate($('.prizes-about'));
+    scrollAnimate($('.prizes-baloon'));
+    scrollAnimate($('.summer-promo'));
+    scrollAnimate($('.summer-map'));
+    
+});   
+
+
+
+function scrollAnimate(element)
+{
+    let windowTop = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    let elementTop = $(element).offset().top;
+    let elementHeight = $(element).outerHeight();
+    let documentHeight = $(document).height();   
+    if (windowTop + windowHeight >= elementTop || windowHeight + windowTop == documentHeight || elementHeight + elementTop  < windowHeight){
+        element.addClass('scroll-animated');
     }
-    
-jQuery(document).ready(function($) {  
-    $(window).scroll(function(){
-
-        element = $('html');
-            let qwe = element.css('font-size');
- 
-            let asd = parseInt(qwe);
-
-        if ($(window).width() >= 1400)
-        {
-
-            if  ($(window).scrollTop() > (asd * 15))
-                $('.prizes-about').addClass('scroll-animated');
-            
-            if  ($(window).scrollTop() > (asd * 15))
-                $('.prizes-baloon').addClass('scroll-animated');
-
-            if  ($(window).scrollTop() > (asd * 30))
-                $('.summer-promo').addClass('scroll-animated');
-    
-            if  ($(window).scrollTop() > (asd * 30))
-                $('.summer-map').addClass('scroll-animated'); 
-        }
-
-        if ($(window).width() >= 1000 && $(window).width() < 1400 )
-        {
-
-            if  ($(window).scrollTop() > (asd * 10))
-                $('.prizes-about').addClass('scroll-animated');
-            
-            if  ($(window).scrollTop() > (asd * 10))
-                $('.prizes-baloon').addClass('scroll-animated');
-
-            if  ($(window).scrollTop() > (asd * 20))
-                $('.summer-promo').addClass('scroll-animated');
-    
-            if  ($(window).scrollTop() > (asd * 20))
-                $('.summer-map').addClass('scroll-animated'); 
-        }
-
-        if ($(window).width() > 800 && $(window).width() < 1000 )
-        {
-
-            if  ($(window).scrollTop() > (asd * 5))
-                $('.prizes-about').addClass('scroll-animated');
-            
-            if  ($(window).scrollTop() > (asd * 5))
-                $('.prizes-baloon').addClass('scroll-animated');
-
-            if  ($(window).scrollTop() > (asd * 10))
-                $('.summer-promo').addClass('scroll-animated');
-    
-            if  ($(window).scrollTop() > (asd * 10))
-                $('.summer-map').addClass('scroll-animated'); 
-        }
-
-        if ($(window).width() > 680 && $(window).width() <= 800 )
-        {
-
-            
-            $('.prizes-about').addClass('scroll-animated');
-        
-        
-            $('.prizes-baloon').addClass('scroll-animated');
-
-            if  ($(window).scrollTop() > (asd * 10))
-                $('.summer-promo').addClass('scroll-animated');
-    
-            if  ($(window).scrollTop() > (asd * 10))
-                $('.summer-map').addClass('scroll-animated'); 
-        }
-        
-        });
-    
-    });   
-   
+}
